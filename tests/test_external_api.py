1b192a154e7e1449abeeca465eb0038eccbe4ee1
verba4182 <verba4182@mail.ru>
2024-07-14 00:45:16 +0300

    hometask 11.1/ 11.2;

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,8 +1,8 @@
 from src.external_api import my_transactions
-from unittest.mock import Mock
+from unittest.mock import patch
 
 
-def test_my_transactions(dict_list_my):
-    mock_random = Mock(return_value=100.0)
-    dict_list_my ['operationAmount']['amount'] = float(mock_random)
-    assert my_transactions(dict_list_my) == 100.0
+@patch('requests.get')
+def test_my_transactions(mock_get, dict_list_my):
+    mock_get.return_value.json.return_value = 9824.07
+    assert my_transactions(dict_list_my) == 9824.07
