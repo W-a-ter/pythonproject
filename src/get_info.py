cbdc6e6d36dcf5a46e11cff2e7be67a56a1d8dee
verba4182 <verba4182@mail.ru>
2024-07-21 15:21:34 +0300

    Hometask fixed 20.07 test_utils added

--- src/get_info.py
+++ src/get_info.py
@@ -2,7 +2,6 @@ import json
 from collections import Counter
 
 
-
 def get_info(my_transaction: list[dict], string_my: str) -> list[dict]:
     counter = []
     for i in my_transaction:
@@ -11,15 +10,13 @@ def get_info(my_transaction: list[dict], string_my: str) -> list[dict]:
     return counter
 
 
-def category_search(transactions: list[dict], counter:list) -> dict:
-    #category = Counter(transactions['description'])
-    #counter = {}
-    my_result = [transaction["description"] for transaction in transactions if transaction.get("description", "")
-                in counter]
+def category_search(transactions: list[dict], counter: list) -> dict:
+    my_result = [transaction["description"] for transaction in
+                 transactions if transaction.get("description", "")
+                 in counter]
     return dict(Counter(my_result))
 
 
 with open('../Data/operations.json', encoding="utf-8") as file:
     info = json.load(file)
 print(category_search(info, 'Перевод'))
-#print (type(info))
